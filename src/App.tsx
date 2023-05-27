a55dfeea63455e101f3c230c3f58bd889df183ec
Gabriel Cesario <gcesario031@gmail.com>
2023-05-27 18:30:40 -0300

    Adicionando Marcadores

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,9 @@
-import React, { useMemo, useState } from 'react';
+import React, { useEffect, useMemo, useState } from 'react';
 import './App.css';
 import Layout, { Button, ButtonStack, Title } from './components/MainLayout';
-import { GoogleMap, useLoadScript } from '@react-google-maps/api';
+import { GoogleMap, InfoWindow, Marker, useLoadScript } from '@react-google-maps/api';
 import clsx from 'clsx';
+import { FaMapMarkerAlt } from 'react-icons/fa';
 
 
 function App() {
@@ -11,9 +12,53 @@ function App() {
   });
 
   const [zoom, setZoom] = useState(15);
+  const [marker, setMarker] = useState<null | JSX.Element>(null);
+  const [mapRef, setMapRef] = useState<null | google.maps.Map>(null);
+  const [openedWindow, setOpenedWindow] = useState<null | number>(null);
+  const [isOpen, setIsOpen] = useState(false);
 
+
+  // useEffect(() => {
+  //   setTimeout(() => { setZoom(z => z - 1); }, 500)
+  // }, []);
+
+  const markers = useMemo(() => [
+    { lat: -15.795299487332993, lng: -47.87307051219112, address: 'Ministério da Educação' },
+    { lat: -15.798197321482878, lng: -47.875550158900936, address: 'Catedral Metropolitana Nossa Senhora Aparecida' },
+    { lat: -15.800601454168254, lng: -47.86730507403445, address: 'Palácio Itamaraty' },
+  ], []);
 
   const center = useMemo(() => ({ lat: -15.795299487332993, lng: -47.87307051219112 }), []);
+
+  const handleToggleMarker = () => {
+    if (marker) {
+      setMarker(null);
+      return;
+    }
+    setMarker(<Marker position={center} draggable />);
+  }
+
+  const handleMapLoad = (someMap: google.maps.Map) => {
+    setMapRef(someMap);
+
+    const bounds = new google.maps.LatLngBounds();
+    markers?.forEach(({ lat, lng }) => bounds.extend({ lat, lng }));
+
+    someMap.fitBounds(bounds);
+  }
+
+  const handleMarkerClick = (point: { lat: number, lng: number }, markerIdx: number) => {
+    mapRef?.panTo(point);
+
+    setOpenedWindow(markerIdx);
+    setIsOpen(true)
+  }
+
+  const handleWindowClose = () => {
+    setOpenedWindow(null);
+    setIsOpen(false);
+  }
+
 
   return (
     <Layout>
@@ -23,11 +68,14 @@ function App() {
       </Title>
 
       <ButtonStack>
-        <Button onClick={() => setZoom(z => z + 1)}>
+        <Button onClick={() => setZoom(z => z + 1)} className='d-none'>
           Aumentar zoom
         </Button>
-        <Button onClick={() => setZoom(z => z - 1)}>
+        <Button onClick={() => setZoom(z => z - 1)} className='d-none'>
           Diminuir Zoom
+        </Button>
+        <Button onClick={handleToggleMarker} className='d-none'>
+          Adicionar Marcador
         </Button>
       </ButtonStack>
 
@@ -38,9 +86,22 @@ function App() {
         (
           <GoogleMap
             mapContainerClassName={clsx('map-container')}
-            center={center}
             zoom={zoom}
-          />
+            onLoad={handleMapLoad}
+          >
+            {markers.map(({ lat, lng, address }, idx) => {
+              const Mark = () => {
+
+                return (
+                  <Marker position={{ lat, lng }} onClick={() => handleMarkerClick({ lat, lng }, idx)}>
+                    { false && <InfoWindow onCloseClick={handleWindowClose}>
+                      <h3 className='color-1'>{address}</h3>
+                    </InfoWindow>}
+                  </Marker>);
+              }
+              return <Mark key={`mark-${idx}`} />;
+            })}
+          </GoogleMap>
         )
       }
     </Layout>
--- src/components/MainLayout/index.tsx
+++ src/components/MainLayout/index.tsx
@@ -1,9 +1,12 @@
 import styled from "styled-components";
 
-
 const Layout = styled.div`
 
-  background-color: #272525;
+  --primary-color: #272525;
+  --secondary-color: rgb(23, 131, 194);
+  --darker-sec-color: rgb(46, 101, 255);
+
+  background-color: var(--primary-color);
   color: #fdf2f2;
 
   --padding: 25px;
@@ -13,22 +16,31 @@ const Layout = styled.div`
   align-items: center;
 
   height: 100vh;
-  width: 100vw;
+  width: 100%;
+
+  .color-1 {
+    color: var(--primary-color);
+  }
 
   .map-container {
     height: 500px;
     width: 75vw;
+
+    border-radius: 15px;
+    border: 3px solid var(--secondary-color);
   }
 
-  padding-top: var(--padding);
-  padding-bottom: var(--padding);
+  .d-none {
+    display: none !important;
+  }
+  padding: var(--padding) 0;
   
 `;
 
 export const Button = styled.button`
 
   color: white;
-  background-color: rgb(46, 101, 255);
+  background-color: var(--secondary-color);
 
   border: none;
   border-radius: 3px;
@@ -36,6 +48,14 @@ export const Button = styled.button`
   padding: 10px;
 
   text-transform: capitalize;
+
+  :focus {
+    background-color: var(--darker-sec-color);
+  }
+
+  :hover {
+    background-color: var(--darker-sec-color);
+  }
 `;
 
 export const Title = styled.h1`
@@ -50,6 +70,7 @@ export const ButtonStack = styled.div`
   display: flex;
   gap: 10px;
   padding-bottom: 20px;
+  flex-wrap: wrap;
 `;
 
 export default Layout;
